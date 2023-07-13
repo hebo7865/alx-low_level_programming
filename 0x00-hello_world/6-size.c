736a7a4698e84366d56a47229b71b0ad156560bd
hebo7865 <lihf95@gmail.com>
2023-07-13 09:04:51 -0700

    New c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  main - size of various types
+ * Return: return 0
+  */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)", sizeof(char));
+	printf("Size of a int: %ld byte(s)", sizeof(int));
+	printf("Size of a long int: %ld byte(s)", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)", sizeof(float));
+	return (0);
+}
