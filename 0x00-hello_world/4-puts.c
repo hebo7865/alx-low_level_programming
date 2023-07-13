697e0d2e79dfdc6951c401acaf802a2acd1b7e0e
hebo7865 <lihf95@gmail.com>
2023-07-13 07:59:42 -0700

    New c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+* main - print a string
+* return: 0
+  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return(0);
 }
