d0c048893ca048661b02788a6750f8288a7fa9e2
hebo7865 <lihf95@gmail.com>
2023-07-14 12:03:24 -0700

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - print the alphabet
+  * Return: return 0
+  */
+int main(void)
+{
+	int i = 122;
+
+		while (i > 97)
+		{
+			i--;
+			putchar(i);
+		}
+		putchar('\n');
+		return (0);
+	}
