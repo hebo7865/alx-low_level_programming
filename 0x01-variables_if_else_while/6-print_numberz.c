b4fc492b7591003e7f0337986c61fe0cf711909f
hebo7865 <lihf95@gmail.com>
2023-07-14 11:01:01 -0700

    Digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - print the digits
+  * Return: return 0
+  */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
