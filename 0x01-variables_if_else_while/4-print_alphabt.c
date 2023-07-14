258a99ec2d6539a947a863a4fd0c8c3e57b66c86
hebo7865 <lihf95@gmail.com>
2023-07-14 10:35:36 -0700

    Not e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - print alphabet except e & q
+  * Return: return 0
+  */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 122)
+	{
+		if (i == 101 || i == 113)
+		{
+			i++;
+			continue;
+		}
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
