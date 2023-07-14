7c67831df404543156df3a03ff7fa965ffb67481
hebo7865 <lihf95@gmail.com>
2023-07-14 10:47:18 -0700

    Digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - print digits
+  * Return: return 0
+  */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		i++;
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
