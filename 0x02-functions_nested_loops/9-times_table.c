8fef3a52128eda8697df56cf53ea7ddc138a45bf
hebo7865 <lihf95@gmail.com>
2023-07-18 15:53:45 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,6 +14,7 @@ void times_table(void)
 		_putchar(',');
 		for (m = 1; m <= 9; m++)
 		{
+			_putchar(',');
 			_putchar(' ');
 			s = (i * m);
 
@@ -26,7 +27,6 @@ void times_table(void)
 				_putchar(' ');
 			}
 			_putchar((s % 10) + 48);
-			_putchar(',');
 		}
 		_putchar('\n');
 	}
