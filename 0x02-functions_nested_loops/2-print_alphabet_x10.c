596c81250058c77c99113422e726e323ec7d6085
hebo7865 <lihf95@gmail.com>
2023-07-18 09:38:18 -0700

    Alphabet 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * print_alphabet_x10 - print the alphabet 10 times
+  * Return: return 0
+  */
+void print_alphabet_x10(void);
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
