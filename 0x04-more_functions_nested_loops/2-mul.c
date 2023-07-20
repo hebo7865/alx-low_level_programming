2c91bb919424a69d4ebe1e8dac80923b168e7740
hebo7865 <lihf95@gmail.com>
2023-07-20 15:10:20 -0700

    multiplie

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * mul - mul a by b
+  *@a: integer input
+  *@b: integer input
+  * Return: always 0
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
