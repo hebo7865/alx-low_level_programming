2e6f3bd0b529a57cc6d4e365ca5d585db299bd34
hebo7865 <lihf95@gmail.com>
2023-07-27 03:45:21 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * cap_string - capitalizes all words of a string
+  *@str: string input
+  * Return: always 0
+  */
+char *cap_string(char *)
+{
+	int i;
+
+
