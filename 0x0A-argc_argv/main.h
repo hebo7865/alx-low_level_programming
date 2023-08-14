54895472c2834600fce1d7c6e1e0e7b396307542
hebo7865 <lihf95@gmail.com>
2023-08-14 15:48:18 -0700

    size

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,8 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-int _putchar(char c);
-
-#endif
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
