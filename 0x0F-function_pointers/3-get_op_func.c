8cf45696e7343afdcfc411050dda25b1ee5568c2
hebo7865 <lihf95@gmail.com>
2023-08-16 17:13:51 -0700

    simple operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -15,11 +15,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-    int i = 0;
+	int i = 0;
 
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
-    {
-	    i++;
-    }
-    return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	{
+		i++;
+	}
+	return (ops[i].f);
 }
