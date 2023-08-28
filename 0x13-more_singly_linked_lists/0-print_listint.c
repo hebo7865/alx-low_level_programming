809cc94618c87953b7e4f67c5043eb7def240c89
hebo7865 <lihf95@gmail.com>
2023-08-28 09:17:49 -0700

    list of element

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+  * print_listint - prints all the elements of a listint_t list
+  * @h: pointer to first element
+  * Return: always 0
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
