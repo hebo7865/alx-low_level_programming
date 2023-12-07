715caa3b137f5aeb502df3361b06d3e36009c8fb
hebo7865 <lihf95@gmail.com>
2023-12-07 21:49:39 +0200

    dlistint len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - the number of elements in a linked dlistint_t list
+ * @h: node header
+ * Return: list size
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
