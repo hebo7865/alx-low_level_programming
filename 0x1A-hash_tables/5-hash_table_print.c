30da42d5a8621598cdc6e145f1b9a09dd8f8d9cf
hebo7865 <lihf95@gmail.com>
2023-12-22 22:24:12 +0200

    hash table print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    ht_item_t *tmp;
+    char data = 0;
+
+    if (ht == NULL || ht->array == NULL)
+    {
+        return;
+    }
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        tmp = ht->array[i];
+        while (tmp != NULL)
+        {
+            if (data == 1)
+            {
+                printf(", ");
+            }
+            printf("'%s': '%s'", tmp->key, tmp->value);
+            data = 1;
+            tmp = tmp->next;
+        }
+    }
+    printf("}\n");
+}
