5c31553e4986b1ec3c39811215acf3d30ca4973e
hebo7865 <lihf95@gmail.com>
2023-12-22 21:39:03 +0200

    hash table get

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: is the hash table you want to look into
+ * @key: is the key you are looking for
+ * Return:  value associated with the element,
+ * NULL if key couldn’t be found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i:
+	ht_item_t *tmp;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+		key == NULL || strlen(key) == 0)
+	{
+		return (NULL);
+	}
+	i = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[i];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
