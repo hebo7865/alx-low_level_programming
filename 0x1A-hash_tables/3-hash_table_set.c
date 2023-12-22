0f34b188003306ec860ca858adc7a19a6f0c5655
hebo7865 <lihf95@gmail.com>
2023-12-22 22:13:32 +0200

    hash table get

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,34 +1,34 @@
 #include "hash_tables.h"
 /**
- * create_hash_node - create new node 
+ * create_hash_node - create new node
  * @key: the key for node
  * @value: the value for key
  * Return: new node or NULL if fail
 */
 ht_item_t *create_hash_node(const char *key, const char *value)
 {
-    ht_item_t *node;
+	ht_item_t *node;
 
-    node = malloc(sizeof(ht_item_t));
-    if (node == NULL)
-    {
-        return (NULL);
-    }
-    node->key = strdup(key);
-    if (node->key == NULL)
-    {
-        free(node);
-        return (NULL);
-    }
-    node->value = strdup(value);
-    if (node->value == NULL)
-    {
-        free(node->key);
-        free(node);
-        return (NULL);
-    }
-    node->next = NULL;
-    return (node);
+	node = malloc(sizeof(ht_item_t));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
 }
 
 /**
@@ -36,16 +36,16 @@ ht_item_t *create_hash_node(const char *key, const char *value)
  * @ht: is the hash table you want to add or update
  * @key: is the key. key can not be an empty string
  * @value: is the value associated with the key
- * Returns: 1 if it succeeded, 0 otherwise
+ * Return: 1 if it succeeded, 0 otherwise
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int i;
-    ht_item_t *node, *tmp;
+	unsigned long int i;
+	ht_item_t *node, *tmp;
 	char *newValue;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0
-	    || key == NULL || strlen(key) == 0 || value == NULL)
+		|| key == NULL || strlen(key) == 0 || value == NULL)
 		{
 			return (0);
 		}
